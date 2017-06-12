964c1241d3437290f7e6cdbf9d7f5eff8f6de7f2
Nazarau.Aliaksandr <nazarau.aliaksandr@gmail.com>
2017-06-12 07:44:22 +0300

    first commit

---
+++ grassian.py
@@ -0,0 +1,29 @@
+import telebot
+import codecs
+import random
+
+bot = telebot.TeleBot("379250925:AAGihZaOJC0Ppl34bg3WZVcc6DhsuRjtBK4")
+
+
+
+fileObj = codecs.open( "grassian.txt", "r", "utf_8_sig" )
+text = fileObj.read()
+
+book = text.split("\r\n")
+book = list(filter(lambda x : len(x) > 2, book))
+
+
+
+upd = bot.get_updates()
+last_update = upd[-1]
+msg = last_update.message
+@bot.message_handler(commands=['start'])
+def handle_text(message):
+    page = random.randint(0, 300)
+    bot.send_message(message.chat.id, book[page])
+
+bot.polling(none_stop=True)
+
+
+
+
